cad6dfe5c80aafc39064b833bc712f2d6c495979
Lprabodha <lprabodha98@gmail.com>
2022-11-13 10:52:25 +0530

    scrap web

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup
+
 
 years = [
     1930,1934,1938,1950,1954,1958,1962,1966,1970,1974,1978,1982,1986,1990,1994,1994,1998,2002,2006,2010,2014,2018
